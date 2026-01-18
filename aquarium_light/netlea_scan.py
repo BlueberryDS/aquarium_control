7fff45e9679c009a47fc9b56971cacc0d0c343bd
Dong Shi <Dong.shi@hotmail.com>
2026-01-17 23:21:19 -0800

    update

--- aquarium_light/netlea_scan.py
+++ aquarium_light/netlea_scan.py
@@ -2,16 +2,19 @@ import asyncio
 from bleak import BleakClient
 
 DEVICE = "50:78:7D:B7:46:62"  # replace with your device address (MAC on Linux/Win; UUID on macOS)
+#DEVICE = "0x1A0111162B2C830150787DB74662"  # replace with your device address (MAC on Linux/Win; UUID on macOS)
 
 async def main():
     async with BleakClient(DEVICE) as client:
-        svcs = await client.get_services()
-        for s in svcs:
+        services = client.services
+        for s in services:
             print("SERVICE", s.uuid)
             for c in s.characteristics:
-                props = ",".join(c.properties)
-                print("  CHAR", c.uuid, f"[{props}]")
-                for d in c.descriptors:
-                    print("    DESC", d.uuid)
+                print(
+                    "  CHAR",
+                    c.uuid,
+                    "props=",
+                    ",".join(c.properties)
+                )
 
 asyncio.run(main())
